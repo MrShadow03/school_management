f622bdfb3e3d127bc871e585dcf44c2a8d554beb
Galib Jaman <gj.emon35@gmail.com>
2023-01-17 18:33:25 +0600

    promo

--- resources/views/components/dashboard/organisms/promotion-form-sm.blade.php
+++ resources/views/components/dashboard/organisms/promotion-form-sm.blade.php
@@ -2,13 +2,12 @@
 <form class="data_form" action="{{ route("admin.routine.store") }}" method="post" onsubmit="submitCreateForm(event, this)">
     @csrf
     <h1 class="form_title">Create a new Section</h1>
-    <div class="input_group d-none">
+    <div class="input_group">
         <div class="input_field">
             <label for="section_id">Class <span class="required">*</span></label>
             <select name="class" id="section_id" onchange="getData(this.value)">
                 
             </select>
-            <p class="input_error d-none" id="class_error"></p>
             @error('section_id')
                 <p class="input_error" id="class_error">{{$message}}</p>
             @enderror
